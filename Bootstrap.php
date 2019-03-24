0c6c2490719013216c7eb6d3ece7e0d844fb14d9
alex-wdmg <wdmg.com.ua@gmail.com>
2019-03-24 21:12:42 +0200

    Add bootstrap

---
+++ Bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace wdmg\reviews;
+
+use yii\base\BootstrapInterface;
+use Yii;
+
+
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        // Get the module instance
+        $module = Yii::$app->getModule('reviews');
+
+        // Get URL path prefix if exist
+        $prefix = (isset($module->routePrefix) ? $module->routePrefix . '/' : '');
+
+        // Add module URL rules
+        $app->getUrlManager()->addRules(
+            [
+                $prefix . '<module:reviews>/' => '<module>/default/index',
+                $prefix . '<module:reviews>/<controller>/' => '<module>/<controller>',
+                $prefix . '<module:reviews>/<controller>/<action>' => '<module>/<controller>/<action>',
+                $prefix . '<module:reviews>/<controller>/<action>' => '<module>/<controller>/<action>',
+            ],
+            true
+        );
+    }
+}
