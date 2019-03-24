a08d34d067c32b3a3bb07107e26c611faa75d611
alex-wdmg <wdmg.com.ua@gmail.com>
2019-03-24 21:12:04 +0200

    Add base module script

---
+++ Module.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace wdmg\reviews;
+
+use Yii;
+
+/**
+ * reviews module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'wdmg\reviews\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $defaultRoute = 'default';
+
+    /**
+     * @var string the prefix for routing of module
+     */
+    public $routePrefix = "admin";
+
+    /**
+     * @var string the vendor name of module
+     */
+    private $vendor = "wdmg";
+
+    /**
+     * @var string the module version
+     */
+    private $version = "0.0.1";
+
+    /**
+     * @var integer, priority of initialization
+     */
+    private $priority = 8;
+
+    /**
+     * @var array of strings missing translations
+     */
+    public $missingTranslation;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // Set controller namespace for console commands
+        if (Yii::$app instanceof \yii\console\Application)
+            $this->controllerNamespace = 'wdmg\reviews\commands';
+
+        // Set current version of module
+        $this->setVersion($this->version);
+
+        // Register translations
+        $this->registerTranslations();
+
+        // Normalize route prefix
+        $this->routePrefixNormalize();
+    }
+
+    /**
+     * Return module vendor
+     * @var string of current module vendor
+     */
+    public function getVendor() {
+        return $this->vendor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function afterAction($action, $result)
+    {
+
+        // Log to debuf console missing translations
+        if (is_array($this->missingTranslation) && YII_ENV == 'dev')
+            Yii::warning('Missing translations: ' . var_export($this->missingTranslation, true), 'i18n');
+
+        $result = parent::afterAction($action, $result);
+        return $result;
+
+    }
+
+    // Registers translations for the module
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['app/modules/reviews'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@vendor/wdmg/yii2-reviews/messages',
+            'on missingTranslation' => function($event) {
+
+                if (YII_ENV == 'dev')
+                    $this->missingTranslation[] = $event->message;
+
+            },
+        ];
+    }
+
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('app/modules/reviews' . $category, $message, $params, $language);
+    }
+
+    /**
+     * Normalize route prefix
+     * @return string of current route prefix
+     */
+    public function routePrefixNormalize()
+    {
+        if(!empty($this->routePrefix)) {
+            $this->routePrefix = str_replace('/', '', $this->routePrefix);
+            $this->routePrefix = '/'.$this->routePrefix;
+            $this->routePrefix = str_replace('//', '/', $this->routePrefix);
+        }
+        return $this->routePrefix;
+    }
+
+    /**
+     * Build dashboard navigation items for NavBar
+     * @return array of current module nav items
+     */
+    public function dashboardNavItems()
+    {
+        return [
+            'label' => Yii::t('app/modules/reviews', 'Reviews'),
+            'url' => [$this->routePrefix . '/reviews/'],
+            'active' => in_array(\Yii::$app->controller->module->id, ['reviews'])
+        ];
+    }
+}
