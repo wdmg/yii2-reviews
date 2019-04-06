db48a4ae7273543229a8dcc6f29758b4411e5ad2
alex-wdmg <wdmg.com.ua@gmail.com>
2019-04-06 16:45:07 +0300

    Added copyrights

--- Module.php
+++ Module.php
@@ -2,6 +2,18 @@
 
 namespace wdmg\reviews;
 
+/**
+ * Yii2 Reviews system
+ *
+ * @category        Module
+ * @version         0.0.2
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @link            https://github.com/wdmg/yii2-reviews
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ *
+ */
+
 use Yii;
 
 /**
