de3cfbf15307261d667ccbce986e7a2edeb66a73
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2019-10-28 00:23:13 +0200

    Added some translations

--- messages/ru-RU/app/modules/reviews.php
+++ messages/ru-RU/app/modules/reviews.php
@@ -3,6 +3,10 @@
 return [
     'Reviews' => "Отзывы",
     'Users reviews system' => "Система отзывов пользователей",
+
+    'No reviews available' => "Нет доступных для отображения отзывов",
+    'An error occurred while retrieving the reviews list' => "Произошла ошибка при получении списка отзывов",
+
 ];
 
 ?>
