34cfbeb712e13ca95353d7f4a21f8fd4568e71dc
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-03-01 01:55:11 +0200

    Change icon class (FontAwesome update)

--- Module.php
+++ Module.php
@@ -75,7 +75,7 @@ class Module extends BaseModule
         $items = [
             'label' => $this->name,
             'url' => [$this->routePrefix . '/'. $this->id],
-            'icon' => 'fa-star-half-o',
+            'icon' => 'fa fa-fw fa-star-half-alt',
             'active' => in_array(\Yii::$app->controller->module->id, [$this->id])
         ];
         return $items;
